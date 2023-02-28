bb497f44cb7de094434dba4375f20c00855d803c
niwantha33 <niwantha33@gmail.com>
2023-02-28 19:06:38 +0000

    updated init and questions.

--- starter/index.js
+++ starter/index.js
@@ -5,15 +5,38 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "title of your project?"
+    },
+
+
+
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+    fs.writeFile(fileName, data, error => {
+        if (error) {
+            throw error;
+        } else {
+            console.log("File created successfully")
+        }
+    });
 }
 
 // function to initialize program
 function init() {
+
+    inquirer.prompt(questions).then(content => {
+
+        const answers = generateMarkdown(content)
+        writeToFile('README.md', answers)
+
+    });
 
 }
 
