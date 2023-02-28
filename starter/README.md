d85b70814024ba84887b9dd8ac4ede02343fc473
niwantha33 <niwantha33@gmail.com>
2023-02-28 19:15:57 +0000

    added md content.

--- starter/README.md
+++ starter/README.md
@@ -1,2 +1,29 @@
-# generated
+
+# project
+
+## Description
+undefined
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+
+
+## Installation
+undefined
+
+## Usage
+undefined
+
+## Contributing
+undefined
+
+## Tests
+undefined
+
+## License
+This project is licensed under the undefined license.
 
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,45 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+  const content = `
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.features}
+
+## How to Contribute
+${data.contribute}
+
+## Tests
+${data.tests}
+
+
 
 `;
+  return content
 }
 
 module.exports = generateMarkdown;
